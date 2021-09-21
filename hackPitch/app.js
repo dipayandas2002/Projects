69d7596bf18ac63bdbb35f29a11565b80e989b9e
@dipayandas2002 <dipayandas360@gmail.com>
2021-09-21 15:04:58 +0530

    3.1

--- hackPitch/app.js
+++ hackPitch/app.js
@@ -1,32 +1,30 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const axios = require("axios")
+const axios = require("axios");
+const sheetdb = require('sheetdb-node');
+let config = {
+    address: 'ieid2rcsjsktv',
+};
+let client = sheetdb(config);
 let mess = []
+let messowner;
+let student;
 
-axios.get('https://sheetdb.io/api/v1/d3veuvq5sboae')
-  .then(function (response) {
-      console.log(response.data);
-      mess = response.data;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+// https://sheetdb.io/api/v1/ieid2rcsjsktv
 
 
 
 
 const mongoose = require('mongoose');
-const { render } = require('ejs');
 mongoose.connect("mongodb+srv://dipu:dipayandas2002@cluster0.74p0f.mongodb.net/originalDB", { useNewUrlParser: true, useUnifiedTopology: true });
 const newSchema = new mongoose.Schema({
     id: Number,
     firstname: String,
-    lastname:String ,
+    lastname: String,
     email: String,
     countryCode: String,
-    phone:String,
+    phone: String,
     jobtitle: String,
     password: String,
     passwordConfirmation: String
@@ -48,7 +46,7 @@ app.get('/login', function (req, res) {
 })
 
 app.get('/register', function (req, res) {
-    res.render("register",{issue : ""});
+    res.render("register", { issue: "" });
 
 
 })
@@ -59,119 +57,126 @@ app.post("/", function (req, res) {
 
 
 
-    // newperson.save();
-
-    //filter
-
-    // Form.find({}, (err, data) => {
-    //     // console.log(data); //fetching all the data
-    //     if (req.body.filter === "on") {
-
-    //         res.render("submit", { array : data , filter : "on" });
-    //     }
-    //     else res.send("no filter")
-    // })
-
-    //  console.log(req.body)
-
-    // Form.findOne({name : "dipu"}, (err, data)=>{
-    //     if (data.email === "dipayandas360@gmail.com") {
-    //         res.send("verified")
-    //     }
-    //     else res.send(data.email)
 
 
-    // })
-
-
-    bcrypt.hash(req.body.name, saltRounds).then(function (hash) {
+    bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
         //    console.log(hash); //hashed password
         let newperson = new Form({
-            name: hash,
-            email: req.body.email
+
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            countryCode: req.body.countryCode,
+            phone: req.body.phone,
+            jobtitle: req.body.jobtitle,
+            password: req.body.password,
+            passwordConfirmation: req.body.passwordConfirmation
         })
 
         newperson.save();
-        render.redirect("/login")
     });
 
+    res.redirect("/login")
 
-    //login route
 
-
-    // Form.findOne({ name: req.body.name }, (err, data) => {
-    //     if (data !== null) {
-    //         bcrypt.compare(req.body.name, data.name).then(function(result) {
-    //            if(result == true){
-    //                res.send("password matched")
-    //            }
-    //         });
-
-    //     }
-    //     else res.send("ok")
-
-    // })
 
 
 })
 
 
-// filter copied from net
-
-app.use('/items', (req, res) => {
-    const filters = req.query;
-    const filteredUsers = data.filter(user => {
-        let isValid = true;
-        for (key in filters) {
-            console.log(key, user[key], filters[key]);
-            isValid = isValid && user[key] == filters[key];
-        }
-        return isValid;
-    });
-    res.send(filteredUsers);
-});
 
 
 
-app.post('/register',(req,res)=>{
-    console.log(req.body);
-    
-    if (req.body.password === req.body.passwordConfirmation ) {
-        
+
+app.post('/register', (req, res) => {
+    // console.log(req.body);
+
+    if (req.body.password === req.body.passwordConfirmation) {
+
         bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
             //    console.log(hash); //hashed password
             let newperson = new Form({
                 firstname: req.body.firstname,
-                lastname:req.body.lastname ,
+                lastname: req.body.lastname,
                 email: req.body.email,
                 countryCode: req.body.countryCode,
                 phone: req.body.phone,
                 jobtitle: req.body.jobtitle,
                 password: hash,
                 passwordConfirmation: req.body.passwordConfirmation
-              })
-    
+            })
+
             newperson.save();
         });
         // res.send("akdm ok ache");
         res.render("login")
     }
     else {
-        res.render("register", {issue : "passwords are not same"})
+        res.render("register", { issue: "passwords are not same" })
     }
 })
 
-app.post("/login",(req,res)=>{
+
+
+app.post("/login", (req, res) => {
 
     Form.findOne({ firstname: req.body.name }, (err, data) => {
         if (data !== null) {
-            bcrypt.compare(req.body.password, data.password).then(function(result) {
-               if(result === true){
-                //    res.send(data) //rendering the data
-                // console.log(mess);
-                
-                res.render("index",{mess : mess})
-               }
+            bcrypt.compare(req.body.password, data.password).then(function (result) {
+                if (result === true) {
+                    //    res.send(data) //rendering the data
+                    // console.log(mess);
+
+                    if (data.jobtitle === "messowner") {
+                        messowner = data.firstname;
+                        client.read({
+                            limit: 1,
+                            search: { 'Name': req.body.name },
+                        }).then(function (data1) {
+
+                            console.log(data1.length);
+
+
+                            if (data1.length === 2) {
+
+                                // res.send("no pg")
+                                client.create({ Name:messowner}).then(function(data) {
+                                    // console.log(data);
+                                  }, function(err){
+                                    console.log(err);
+                                  });
+                                res.redirect("/addpg")
+                            }
+                            else {
+
+                                let json = JSON.parse(data1)
+                                res.render("profile", {
+                                    fname: data.firstname, lname: data.lastname, email: data.email, phonenumber: data.phone, element: json[0]
+
+                                })
+                                messowner = data.firstname;
+                                // console.log(json);
+                                // messdata = data1;
+                            }
+                        }, function (err) {
+                            console.log(err);
+                        });
+                        // console.log(messdata)
+                    }
+                    else {
+                        axios.get('https://sheetdb.io/api/v1/ieid2rcsjsktv')
+                            .then(function (response) {
+                                //   console.log(response.data);
+                                mess = response.data;
+                            })
+                            .catch(function (error) {
+                                // handle error
+                                console.log(error);
+                            })
+                        student = data;
+                        res.render("index", { mess: mess, sname: student.firstname + " " + student.lastname, semail: student.email, snumber: student.phone })
+                    }
+                }
             });
 
         }
@@ -183,20 +188,29 @@ app.post("/login",(req,res)=>{
 
 
 
-app.post("/index",(req,res)=>{
+app.post("/index", (req, res) => {
 
-    // console.log(req.body);
-   let {gender,parking,food,occupancy} = req.body
-   
-   let newfilter = mess.filter((element,index)=>{
-        let {Gender,Parking,Food,Occupancy} = element
+    axios.get('https://sheetdb.io/api/v1/ieid2rcsjsktv')
+        .then(function (response) {
+            //   console.log(response.data);
+            mess = response.data;
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+    console.log(req.body);
+    let { gender, parking, food, occupancy } = req.body
 
-        
+    let newfilter = mess.filter((element, index) => {
+        let { Gender, Parking, Food, Occupancy } = element
 
-       if (( food === "none" || food === Food ) &&  ( gender === "none" || gender === Gender ) && ( parking === "none" || parking === Parking )  && ( occupancy === "none" || occupancy === Occupancy )){
-           return true;
-       };
-        
+
+
+        if ((food === "none" || food === Food) && (gender === "none" || gender === Gender) && (parking === "none" || parking === Parking) && (occupancy === "none" || occupancy === Occupancy)) {
+            return true;
+        };
+
 
     });
 
@@ -205,13 +219,110 @@ app.post("/index",(req,res)=>{
             return a.Price - b.Price;
         });
     }
-    else if(req.body.sort === "rating"){
+    else if (req.body.sort === "rating") {
         newfilter.sort((a, b) => {
-            return b.Rating - a.Rating;
+            return a.Rating - b.Rating;
         });
     }
 
     // res.send(req.body)
-    res.render('index',{mess:newfilter})
+    res.render('index', { mess: newfilter, sname: student.firstname + " " + student.lastname, semail: student.email, snumber: student.phone })
 
 })
+
+
+app.post("/messowner", (req, res) => {
+    // console.log(req.body);
+
+    let { Location, Highlight, Food, Gender, Occupancy, Parking, Notice, Distance, Eastablished, Deposit, Price, Contact } = req.body
+
+    let array = ["Location", "Highlight", "Food", "Gender", "Occupancy", "Parking", "Notice", "Distance", "Eastablished", "Deposit", "Price", "Contact"]
+
+    let arraydata = [Location, Highlight, Food, Gender, Occupancy, Parking, Notice, Distance, Eastablished, Deposit, Price, Contact]
+
+    // let object = {
+    //     id: '',
+    //     Name: '',
+    //     Highlight: '',
+    //     Location: '',
+    //     Contact: '',
+    //     Gender: '',
+    //     Occupancy: '',
+    //     Description: '',
+    //     Distance: '',
+    //     Rating: '',
+    //     Price: '',
+    //     Images: '',
+    //     Deposit: '',
+    //     Notice: '',
+    //     Food: '',
+    //     Parking: '',
+    //     Eastablished: ''
+    // }
+    let object1 = {}
+    for (let i = 0; i < array.length; i++) {
+        if (arraydata[i] !== "") {
+
+            object1[array[i]] = arraydata[i]
+        }
+
+    }
+    console.log(object1);
+
+    client.update(
+        'Name', // column name
+        messowner, // value to search for
+         object1 // object with updates
+        
+      ).then(function(data) {
+        console.log(data);
+        res.redirect("/messowner")
+      }, function(err){
+        console.log(err);
+      });
+})
+
+
+app.get("/messowner", (req, res) => {
+    client.read({
+        limit: 1,
+        search: { 'Name': messowner },
+    }).then(function (data2) {
+
+        let json1 = [{
+            id: '',
+            Name: '',
+            Highlight: '',
+            Location: '',
+            Contact: '',
+            Gender: '',
+            Occupancy: '',
+            Description: '',
+            Distance: '',
+            Rating: '',
+            Price: '',
+            Images: '',
+            Deposit: '',
+            Notice: '',
+            Food: '',
+            Parking: '',
+            Eastablished: ''
+        }];
+
+        json1 = JSON.parse(data2)
+        res.render("profile", {
+            fname: json1[0].Name, lname: "", email: "", phonenumber: json1[0].Contact, element: json1[0]
+
+        })
+        // messowner = data.firstname;
+        console.log(json1);
+        // messdata = data1;
+    }, function (err) {
+        console.log(err);
+    });
+})
+
+app.get("/addpg", (req, res) => {
+    res.render("addpg", { fname:messowner, lname: "", email: "", phonenumber: "", element: "" })
+})
+
